f2622ba24c253b9e3ffd17d8e84a519fa2a63bad
Vieh <sirvieh@gmail.com>
2017-05-13 22:21:59 +0200

    Changed input method + added log colors

--- Lotek/Program.cs
+++ Lotek/Program.cs
@@ -3,17 +3,17 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Xml.Serialization;
 
 namespace Lotek
 {
-    class Program
+    internal class Program
     {
-        static LuckyNumbers luckyNumbers = new LuckyNumbers();
+        private static LuckyNumbers luckyNumbers = new LuckyNumbers();
         private static Activation activation = new Activation();
-        static void Main(string[] args)
+
+        private static void Main(string[] args)
         {
             if (!File.Exists("activaction.xml"))
             {
@@ -36,12 +36,11 @@ namespace Lotek
             {
                 luckyNumbers.Numbers = new List<List<int>>();
                 ReadFromFile();
-                int readedLocalNumber = 0;
-                ConsoleKeyInfo choice = new ConsoleKeyInfo();
+                string[] enteredNumbersString;
+                var choice = new ConsoleKeyInfo();
                 bool exists = false;
                 try
                 {
-
                 }
                 catch (Exception e)
                 {
@@ -56,74 +55,98 @@ namespace Lotek
 
                     choice = Console.ReadKey();
                     Console.WriteLine("");
-                    List<int> numbersToSave = new List<int>();
+                    var numbersToSave = new List<int>();
                     switch (choice.Key)
                     {
                         case ConsoleKey.D1:
-                            for (int i = 0; i < 6; i++)
+                            Console.WriteLine("Podaj 6 liczb oddzielonych spacjami, aby dodać losowanie:");
+                            enteredNumbersString = Regex.Split(Console.ReadLine(), " ");
+                            if (enteredNumbersString.Count() < 6 || enteredNumbersString.Count() > 6)
                             {
-                                int num = i + 1;
-                                Console.WriteLine("Podaj liczbę [" + num + "] :");
-                                try
-                                {
-                                    readedLocalNumber = Convert.ToInt32(Console.ReadLine());
-                                }
-                                catch (FormatException)
-                                {
-                                    Console.WriteLine("To nie jest liczba. Spróbuj ponownie.");
-                                    readedLocalNumber = -1;
-                                }
-                                if (readedLocalNumber < 1 || readedLocalNumber > 49)
-                                {
-                                    if (readedLocalNumber != -1)
-                                        Console.WriteLine("Tej liczby nie ma w Lotku. Wpisz jeszcze raz.");
-                                    i--;
-                                }
-                                else
-                                {
-                                    foreach (int nToSave in numbersToSave)
-                                    {
-                                        if (nToSave == readedLocalNumber)
-                                        {
-                                            exists = true;
-                                        }
-                                    }
-                                    if (exists)
-                                    {
-                                        Console.WriteLine("Nie można podać dwóch takich samych liczb w jednym losowaniu");
-                                        i--;
-                                        exists = false;
-                                    }
-                                    else
-                                    {
-                                        numbersToSave.Add(readedLocalNumber);
-                                    }
-                                }
-                            }
-                            if (!CheckIfExists(numbersToSave))
-                            {
-                                Console.WriteLine("Zapisuję podane liczby...");
-                                luckyNumbers.Numbers.Add(numbersToSave);
-                                Console.WriteLine("Ilość losowań: " + luckyNumbers.Numbers.Count);
-                                foreach (List<int> currentNumbers in luckyNumbers.Numbers)
-                                {
-                                    foreach (int i in currentNumbers)
-                                    {
-                                        Console.Write(i + "/");
-                                    }
-                                    Console.WriteLine("");
-                                }
+                                Console.Clear();
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine("Podano złą ilość cyfr, bądź w złym formacie.");
+                                Console.ResetColor();
                             }
                             else
                             {
-                                Console.WriteLine("Podane losowanie już istnieje.");
+                                int numberBeforeAdd = -1;
+                                foreach (string stringNumber in enteredNumbersString)
+                                {
+                                    try
+                                    {
+                                        numberBeforeAdd = Convert.ToInt32(stringNumber);
+                                    }
+                                    catch (FormatException)
+                                    {
+                                        Console.Clear();
+                                        Console.ForegroundColor = ConsoleColor.Red;
+                                        Console.WriteLine("Jedna z wartości nie jest liczbą. Spróbuj ponownie.");
+                                        Console.ResetColor();
+                                        numberBeforeAdd = -1;
+                                        break;
+                                    }
+                                    if (numberBeforeAdd < 1 || numberBeforeAdd > 49)
+                                    {
+                                        if (numberBeforeAdd != -1)
+                                        {
+                                            Console.Clear();
+                                            Console.ForegroundColor = ConsoleColor.Red;
+                                            Console.WriteLine("Tej liczby nie ma w Lotku. Podaj losowanie jeszcze raz.");
+                                            Console.ResetColor();
+                                        }
+                                    }
+                                    else
+                                    {
+                                        numbersToSave.Add(numberBeforeAdd);
+                                    }
+                                }
+                                if (numbersToSave.GroupBy(n => n).Any(c => c.Count() > 1))
+                                {
+                                    exists = true;
+                                }
+                                if (exists)
+                                {
+                                    Console.Clear();
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine(
+                                        "Nie można podać dwóch takich samych liczb w jednym losowaniu");
+                                    Console.ResetColor();
+                                    exists = false;
+                                    break;
+                                }
+                                if (!CheckIfExists(numbersToSave))
+                                {
+                                    Console.Clear();
+                                    Console.ForegroundColor = ConsoleColor.Green;
+                                    Console.WriteLine("Zapisano losowanie");
+                                    Console.ForegroundColor = ConsoleColor.Yellow;
+                                    luckyNumbers.Numbers.Add(numbersToSave);
+                                    Console.WriteLine("Ilość losowań: " + luckyNumbers.Numbers.Count);
+                                    foreach (var currentNumbers in luckyNumbers.Numbers)
+                                    {
+                                        foreach (int i in currentNumbers)
+                                        {
+                                            Console.Write(i + "/");
+                                        }
+                                        Console.WriteLine("");
+                                    }
+                                    Console.ResetColor();
+                                }
+                                else
+                                {
+                                    Console.Clear();
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine("Podane losowanie już istnieje.");
+                                    Console.ResetColor();
+                                }
+                                SaveToFile(luckyNumbers);
                             }
                             break;
                         case ConsoleKey.D2:
                             DisplayReport();
                             break;
                         case ConsoleKey.D3:
-                            SaveToFile(luckyNumbers);
                             break;
                     }
                 }
@@ -143,7 +166,7 @@ namespace Lotek
         private static void DisplayReport()
         {
             int threes = 0, fours = 0, fives = 0, sixes = 0;
-            List<int> recentNumbers = new List<int>();
+            var recentNumbers = new List<int>();
             int readedLocalNumber;
             bool exists = false;
             Console.WriteLine("Podaj kolejno liczby z ostatniego losowania");
@@ -157,13 +180,21 @@ namespace Lotek
                 }
                 catch (FormatException)
                 {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
                     Console.WriteLine("To nie jest liczba. Spróbuj ponownie.");
+                    Console.ResetColor();
                     readedLocalNumber = -1;
                 }
                 if (readedLocalNumber < 1 || readedLocalNumber > 49)
                 {
                     if (readedLocalNumber != -1)
+                    {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Tej liczby nie ma w Lotku. Wpisz jeszcze raz.");
+                        Console.ResetColor();
+                    }
                     i--;
                 }
                 else
@@ -177,7 +208,10 @@ namespace Lotek
                     }
                     if (exists)
                     {
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Red;
                         Console.WriteLine("Nie można podać dwóch takich samych liczb w jednym losowaniu");
+                        Console.ResetColor();
                         i--;
                         exists = false;
                     }
@@ -187,7 +221,7 @@ namespace Lotek
                     }
                 }
             }
-            foreach (List<int> currentNumbers in luckyNumbers.Numbers)
+            foreach (var currentNumbers in luckyNumbers.Numbers)
             {
                 int countedNumbers = 0;
                 bool isFound;
@@ -214,24 +248,29 @@ namespace Lotek
                     case 6:
                         sixes++;
                         break;
-                    default:
-                        Console.WriteLine("Zła ilość znaleziona = " + countedNumbers);
-                        break;
+                        //default:
+                        //    Console.ForegroundColor = ConsoleColor.Red;
+                        //    Console.WriteLine("Zła ilość znaleziona = " + countedNumbers);
+                        //    Console.ResetColor();
+                        //    break;
                 }
                 Console.WriteLine("");
             }
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("*******************RAPORT*********************");
             Console.WriteLine("Ilość trójek: " + threes);
             Console.WriteLine("Ilość czwórek: " + fours);
             Console.WriteLine("Ilość piątek: " + fives);
             Console.WriteLine("Ilość szóstek: " + sixes);
             Console.WriteLine("*******************GRATULACJE*********************");
+            Console.ResetColor();
         }
 
         public static bool CheckIfExists(List<int> list)
         {
             int found = 0;
-            foreach (List<int> currentNumbers in luckyNumbers.Numbers)
+            foreach (var currentNumbers in luckyNumbers.Numbers)
             {
                 found = 0;
                 foreach (int i in currentNumbers)
@@ -244,38 +283,40 @@ namespace Lotek
                         }
                     }
                 }
-            }
-            if (found == 6)
-            {
-                return true;
+                if (found == 6)
+                {
+                    return true;
+                }
             }
             return false;
         }
 
         public static void SaveToFile(LuckyNumbers lNumbers)
         {
-            XmlSerializer xml = new XmlSerializer(typeof(LuckyNumbers));
-            using (Stream fStream = new FileStream("data.xml", FileMode.Create, FileAccess.Write , FileShare.None))
+            var xml = new XmlSerializer(typeof (LuckyNumbers));
+            using (Stream fStream = new FileStream("data.xml", FileMode.Create, FileAccess.Write, FileShare.None))
             {
                 xml.Serialize(fStream, lNumbers);
             }
         }
+
         public static void SaveToFileAct()
         {
-            XmlSerializer xml = new XmlSerializer(typeof(Activation));
+            var xml = new XmlSerializer(typeof (Activation));
             using (Stream fStream = new FileStream("ACT.xml", FileMode.Create, FileAccess.Write, FileShare.None))
             {
                 xml.Serialize(fStream, activation);
             }
         }
+
         public static void ReadFromFile()
         {
-            XmlSerializer xml = new XmlSerializer(typeof(LuckyNumbers));
+            var xml = new XmlSerializer(typeof (LuckyNumbers));
             try
             {
                 using (Stream fStream = new FileStream("data.xml", FileMode.Open, FileAccess.Read, FileShare.None))
                 {
-                    luckyNumbers = (LuckyNumbers)xml.Deserialize(fStream);
+                    luckyNumbers = (LuckyNumbers) xml.Deserialize(fStream);
                 }
             }
             catch (FileNotFoundException)
@@ -283,14 +324,16 @@ namespace Lotek
                 Console.WriteLine("Brak pliku z losowaniami. Tworzę nowy.");
             }
         }
+
         public static void ReadFromActivationFile()
         {
-            XmlSerializer xml = new XmlSerializer(typeof(Activation));
+            var xml = new XmlSerializer(typeof (Activation));
             try
             {
-                using (Stream fStream = new FileStream("activaction.xml", FileMode.Open, FileAccess.Read, FileShare.None))
+                using (
+                    Stream fStream = new FileStream("activaction.xml", FileMode.Open, FileAccess.Read, FileShare.None))
                 {
-                    activation = (Activation)xml.Deserialize(fStream);
+                    activation = (Activation) xml.Deserialize(fStream);
                 }
             }
             catch (FileNotFoundException)
@@ -305,7 +348,8 @@ namespace Lotek
             Console.WriteLine("Proszę czekać...");
             using (var client = new WebClient())
             {
-                client.DownloadFile("https://docs.google.com/uc?export=download&id=0B-axT0J2c_GhR0pRWDhhLWdFcjQ", "activaction.xml");
+                client.DownloadFile("https://docs.google.com/uc?export=download&id=0B-axT0J2c_GhR0pRWDhhLWdFcjQ",
+                    "activaction.xml");
             }
             Console.WriteLine("Gotowe");
         }
